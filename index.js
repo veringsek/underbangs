088830d6e8c337979376b44a736a2cb230898cf9
unknown <veringsek@gmail.com>
2019-02-14 04:05:46 +0800

    me: store player's info client: the express-server as a player lobby: store game's info at the aspect of this player game: store game-server's info like game progress or player list server: the express-server as game-server

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const IP = require("ip");
 const express = require("express");
 
-const PORT_DEFAULT = 9487;
+const PORT_SERVER = 9487;
+const PORT_CLIENT = 9488;
 
-lobby = {};
+let serve = function (port, gets) {
+    handler = express();
+    if (typeof gets === "function") {
+        gets = { "/": gets };
+    }
+    for (let route in gets) {
+        handler.get(route, gets[route]);
+    }
+    handler.listen(port);
+    return handler;
+};
 
-let join = (ip, port = PORT_DEFAULT) => {
+let me = {
+    ip: IP.address(),
+    port: PORT_CLIENT,
+    name: "unknown-player"
+};
+let client = serve(me.port, (req, res) => {
+    res.send(`im player ${me.name}`);
+});
+let lobby = {}; // info about the game we joined
+
+let join = (ip, port = PORT_SERVER) => {
     lobby.ip = ip;
     lobby.port = port;
 };
 
-let host = (port = PORT_DEFAULT) => {
-    lobby.ip = IP.address();
-    lobby.port = port;
-    let server = express();
-    server.get(`/`, (req, res) => {
-        res.send(`kkk`);
+let host = (port = PORT_SERVER) => {
+    game = {
+        ip: IP.address(),
+        port: port,
+        host: me.ip,
+        stage: "wait"
+    };
+    server = serve(game.port, {
+        "/": (req, res) => {
+            res.send(`kkk`);
+        }
     });
-    server.listen(lobby.port);
+
+    join(game.ip, game.port);
 };
