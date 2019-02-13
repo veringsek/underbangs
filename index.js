e08a5df30097f13cb7bfef707f86ced3bb5f9da3
unknown <veringsek@gmail.com>
2019-02-14 01:18:27 +0800

    welcome ui simple

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const ip = require("ip");
+const IP = require("ip");
 const express = require("express");
+
+const PORT_DEFAULT = 9487;
+
+lobby = {};
+
+let join = (ip, port = PORT_DEFAULT) => {
+    lobby.ip = ip;
+    lobby.port = port;
+};
+
+let host = (port = PORT_DEFAULT) => {
+    lobby.ip = IP.address();
+    lobby.port = port;
+    let server = express();
+    server.get(`/`, (req, res) => {
+        res.send(`kkk`);
+    });
+    server.listen(lobby.port);
+};
